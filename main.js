2d56339c82a2d3fda5a9a6693e8d396f188813b9
David Sanchez <davidendubai@gmail.com>
2023-07-06 18:07:07 -0500

    [+] Solucion 3 ejercicio taller final BIT

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 //*****************TALLER DE EJERCICIOS BIT 06/07/2023************/
 
-/** 2. Sumar valores de una lista Escribe una función que tome una lista de números 
- * y devuelva la suma de los números. Los números pueden ser negativos o no enteros. 
- * Si la lista no contiene ningún número, debe devolver 0.
-Resultados esperados */
+/* 3. ¿Qué tan bueno eres realmente?*
+Hubo un examen en tu clase y lo pasaste. ¡Felicidades! Pero eres una persona ambiciosa.
+Quiere saber si es mejor que el estudiante promedio de su clase.
+Recibe una lista con los puntajes de las pruebas de sus compañeros.
+¡Ahora calcule el promedio y compare su puntaje!
+¡Devuelve True si estás mejor, de lo contrario False!
+Nota: Sus puntos no están incluidos en la lista de puntos de su clase. 
+¡Para calcular el punto promedio, puede agregar su punto
+a la lista dada!
+*/
 
-function sumarLista (numeros) {
+function mejorPuntaje(puntajes, tuPuntaje) {
     let suma = 0;
-    for (let i = 0; i< numeros.length; i++)
-    suma += numeros [i];
+    let totalPuntajes = puntajes.length + 1;
+    for (let i = 0; i < puntajes.length; i++) {
+        suma += puntajes[i];
+    }
+    let promedioClase = (suma + tuPuntaje) / totalPuntajes;
+    return tuPuntaje > promedioClase
 }
 
-    return suma;
-
-let lista1 = [1, 5.2, 4, 0, -1];
-let resultado1 = sumarLista(lista1);
-console.log(resultado1);
-
-let lista2 = [];
-let resultado2 = sumarLista(lista2);
-console.log(resultado2);
-
-let lista3 = [-2.398];
-let resultado3 = sumarLista(lista3);
-console.log(resultado3);
-                                          
+let puntajesCompaneros = [3.5, 2.8, 4.5];
+let tuPuntaje = 4.6;
+let resultado = mejorPuntaje(puntajesCompaneros, tuPuntaje)
+console.log(resultado);
